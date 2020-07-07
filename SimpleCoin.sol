ada5214af043e83267960ba89cab4a5066e815b2
김연욱 / YeonUk Kim <yeonuk44@gmail.com>
2020-07-06 20:12:09 -0700

    Remix IDE 를 통한 Deploy
    JavaScript VM 기반 시뮬레이터에서만 코드를 실행시켜보았으며, 테스트 계정을 통한 Balance 전송까지만 구현.

---
+++ SimpleCoin.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+contract SimpleCoin {
+    
+    mapping (address => uint256) public coinBalance;
+    
+    constructor() public {
+        coinBalance
+        [0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C] = 10000;
+    }
+    
+    
+    
+    function transfer(address _to, uint256 _amount) public {
+        coinBalance[msg.sender] -= _amount;
+        coinBalance[_to] += _amount;
+    }
+}
