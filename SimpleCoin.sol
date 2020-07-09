ee7b6d6cddb32c7515f88acd8a63970f3bad1ffb
김연욱 / YeonUk Kim <yeonuk44@gmail.com>
2020-07-08 21:09:06 -0700

    SimpleCoin Improvement
    전달 함수에서 입력 유효성 검사가 부족함.
    개선) 
    1. 생성자에 매개변수를 추가하여 초기 발행되는 토큰이 테스트 계정의 주소로 전달되지 않고 계약 소유자의 계정 주소로 할당되도록 하였음. 
    2. 전송 함수 실행 시 부정한 전송을 막기 위한 몇 가지 로직을 추가하였음.
    3. 토큰 전송 시 이벤트를 발생시켜 스마트 컨트랙트의 클라이언트에 알려주거나 이에 반응하도록 설정하였음.

--- SimpleCoin.sol
+++ SimpleCoin.sol
@@ -4,15 +4,20 @@ contract SimpleCoin {
     
     mapping (address => uint256) public coinBalance;
     
-    constructor() public {
-        coinBalance
-        [0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C] = 10000;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    constructor(uint256 _initialSupply) public {
+        coinBalance[msg.sender] = _initialSupply;
     }
     
     
     
     function transfer(address _to, uint256 _amount) public {
+        require(coinBalance[msg.sender] >= _amount);
+        require(coinBalance[_to] + _amount 
+            >= coinBalance[_to]);
         coinBalance[msg.sender] -= _amount;
         coinBalance[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
     }
 }
