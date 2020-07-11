8027b5101a69f769c998358c90a6ec0be1711042
김연욱 / YeonUk Kim <yeonuk44@gmail.com>
2020-07-10 19:49:41 -0700

    SimpleCoin Improvement #01
    기존 제공하던 기능이 부실함.
    개선)
    1. 이체 허용량을 설정하는 기능을 중첩 매핑을 통한 모델링하여 구현 하였음. 한 계정에서 여러 계정에 지정된 수의 토큰만큼 관리할 수 있도록 허용 하였음.
    2. 코인 발행을 소유자가 변수를 초기화한 후 컨트랙트 소유자만 호출할 수 있도록 하여 함수 실행을 제한할 수 있게 하였음. 
    3. 이제 컨트랙트 소유자의 권한을 더욱 확장하고 계정을 동결할 수 있는 독점권을 부여할 수 있게 하였음. 불(bool) 형 매개변수의 값에 따라 함수를 동결하거나 해제하되 함수제어자인 onlyOwner 를 사용해 로직을 재사용할 수 있도록 캡슐화 하였음.

--- SimpleCoin.sol
+++ SimpleCoin.sol
@@ -3,21 +3,59 @@ pragma solidity ^0.4.0;
 contract SimpleCoin {
     
     mapping (address => uint256) public coinBalance;
+    mapping (address => mapping (address => uint256)) public allowance;
+    mapping (address => bool) public frozenAccount;
+    address public owner;
     
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event FrozenAccount(address target, bool frozen);
     
-    constructor(uint256 _initialSupply) public {
-        coinBalance[msg.sender] = _initialSupply;
+    modifier onlyOwner {
+        if (msg.sender != owner) revert();
+        _;
     }
     
-    
+    constructor(uint256 _initialSupply) public {
+        owner = msg.sender;
+        mint(owner, _initialSupply);
+    }
     
     function transfer(address _to, uint256 _amount) public {
+        require(_to != 0x0);
         require(coinBalance[msg.sender] >= _amount);
         require(coinBalance[_to] + _amount 
             >= coinBalance[_to]);
         coinBalance[msg.sender] -= _amount;
         coinBalance[_to] += _amount;
+        
         emit Transfer(msg.sender, _to, _amount);
+    }
+    
+    function authorize(address _authorizedAccount, uint256 _allowance) public returns (bool success) {
+        allowance[msg.sender][_authorizedAccount] = _allowance;
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
+        require(_to != 0x0);
+        require(coinBalance[_from] > _amount);
+        require(coinBalance[_to] + _amount >= coinBalance[_to]);
+        require(_amount <= allowance[_from][msg.sender]);
+        
+        coinBalance[_from] -= _amount;
+        coinBalance[_to] += _amount;
+        allowance[_from][msg.sender] -= _amount;
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+    
+    function mint(address _recipient, uint256 _mintedAmount) onlyOwner public {
+        coinBalance[_recipient] += _mintedAmount;
+        emit Transfer(owner, _recipient, _mintedAmount);
+    }
+    
+    function freezeAccount(address target, bool freeze) onlyOwner public {
+        frozenAccount[target] = freeze;
+        emit FrozenAccount(target, freeze);
     }
 }
