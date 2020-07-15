7ddf0a00e265f77ae5acbbe974d0deb183e6bda3
김연욱 / YeonUk Kim <yeonuk44@gmail.com>
2020-07-15 12:36:36 +0900

    SimpleCoin Improvement #04
    1. SimpleCoin이 ERC20 표준과  호환하지 않는 문제 발생
    개선) ERC20 표준에 완전히 호환하는 토큰으로 SimpleCoin.sol 을 리팩토링하였음.
    
    2. ERC20 wiki 에서 권장하는 사항인 토큰 초기화 (eip-20)을 발견
    개선) 토큰 생성자가 인터페이스의 일부가 아니므로 표준에 포함되지 않으나 ERC20.sol 에 유의미한 정보로 초기화하는 코드를 추가하였음.

--- ERC20.sol
+++ ERC20.sol
@@ -1,19 +1,18 @@
 pragma solidity ^0.4.24;
 
 contract ERC20 {
+    string public constant name = "Token Name";
+    string public constant symbol = "KYU";
+    
+    uint8 public constant decimals = 18;
     uint256 public totalSupply;
     
     function balanceOf(address _owner) public view returns (uint256 balance);
-    
     function transfer(address _to, uint256 _value) public returns (bool success);
-    
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    
     function approve(address _owner, address _spender) public returns (bool success);
-    
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
     
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
